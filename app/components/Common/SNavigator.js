5d2bd5a6a8bcb502d5a1e3c3a4b4f10e01b6c7f3
jajul <jajulnex@mail.ru>
2017-03-12 01:03:33 +0300

    Fix my mistake

--- app/components/Common/SNavigator.js
+++ app/components/Common/SNavigator.js
@@ -8,8 +8,13 @@ import {OS} from 'app/constants'
  * функция app/route/goTo служит для перехода между экранами
  */
 export default class SNavigator extends Component {
+    /**
+     * initialRoute - Начальный экран навигатора, ожидается {index, component, passProps}
+     * scenes - Наименования сцен в формате {[component]: title} (см. app/route/scenes)
+     */
     propTypes: {
-        initialRoute: React.propTypes.object
+        initialRoute: React.propTypes.object,
+        scenes: React.propTypes.object,
     }
 
     render(){
@@ -75,8 +80,10 @@ export default class SNavigator extends Component {
 
     // TODO Разместить по центру по горизонтали
     _renderTitle = (route, navigator, index, navState) => {
+        const {scenes} = this.props;
+        const title = route.title || scenes[route.component].title || 'Notitle'
         return (
-            <Text style={styles.title}>{route.title}</Text>
+            <Text style={styles.title}>{title}</Text>
         )
     }
 }
@@ -102,3 +109,4 @@ const styles = StyleSheet.create({
         justifyContent: 'center'
     }
 })
+
