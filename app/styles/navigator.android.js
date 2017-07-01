70f0e6aba57afc5244879a84979fe44a41fafbfb
jajulnex@mail.ru <jajulnex@mail.ru>
2017-07-01 15:58:16 +0300

    Убраны лишние стили из navigatorStyles. Для андроида поправлен marginTop.

--- app/styles/navigator.android.js
+++ app/styles/navigator.android.js
@@ -1,24 +1,7 @@
 import {StyleSheet} from 'react-native'
 
 export const navigatorStyles = StyleSheet.create({
-    navigationBar: {
-        backgroundColor: '#009E8E',
-        height: 50,
-    },
     scene: {
-        marginTop: 50,
+        marginTop: 0,
     },
-    leftNavBtn: {
-        marginLeft: 5
-    },
-    rightNavBtn: {
-        marginRight: 5
-    },
-    title: {
-        marginTop: 30,
-        marginLeft: 30,
-        fontSize: 16,
-        justifyContent: 'center',
-        color: 'white',
-    }
 })
